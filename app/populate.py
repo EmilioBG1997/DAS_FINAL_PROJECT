79ff0096a1d13c2db1614af010498983e48d0bad
Fernando Garcia <luis_morales@uadec.edu.mx>
2020-06-04 15:58:57 -0500

    CAMBIO PUERTO PGADMIN

--- app/populate.py
+++ app/populate.py
@@ -7,30 +7,30 @@ def main():
     data = api.get_data()
 
     for i in data:
-        try: 
-            occupation = i.get("work",{}).get("occupation")
-        except:
-            occupation = "None"
-        Super_heroe.get_or_create(
-            name = i.get("name"),
-            full_name = i.get("biography",{}).get("full-name"),
-            alter_egos = i.get("biography",{}).get("alter-egos"),
-            aliases = i.get("biography",{}).get("aliases")[0],
-            place_of_birth = i.get("biography",{}).get("place-of-birth"),
-            first_appearance = i.get("biography",{}).get("first-appearance"),
-            publisher = i.get("biography",{}).get("publisher"),
-            alignment = i.get("biography",{}).get("alignment"),
-            gender = i.get("appearance",{}).get("gender"),
-            race = i.get("appearance",{}).get("race"),
-            height = i.get("appearance",{}).get("height")[1],
-            weight = i.get("appearance",{}).get("weight")[1],
-            eye_color = i.get("appearance",{}).get("eye-color"),
-            hair_color = i.get("appearance",{}).get("hair-color"),
-            occupation = occupation,
-            base = i.get("work",{}).get("base"),
-            group_affiliation = i.get("connections",{}).get("group-affiliation"),
-            relatives = i.get("connections",{}).get("relatives"),
-            image = i.get("image",{}).get("url")
-        )
+        try:
+            dc = SUPER_HEROE.get_or_create(
+                name = i.get("name"),
+                full_name = i.get("biography",{}).get("full-name"),
+                alter_egos = i.get("biography",{}).get("alter-egos"),
+                aliases = i.get("biography",{}).get("aliases")[0],
+                place_of_birth = i.get("biography",{}).get("place-of-birth"),
+                first_appearance = i.get("biography",{}).get("first-appearance"),
+                publisher = i.get("biography",{}).get("publisher"),
+                alignment = i.get("biography",{}).get("alignment"),
+                gender = i.get("appearance",{}).get("gender"),
+                race = i.get("appearance",{}).get("race"),
+                height = i.get("appearance",{}).get("height")[1],
+                weight = i.get("appearance",{}).get("weight")[1],
+                eye_color = i.get("appearance",{}).get("eye-color"),
+                hair_color = i.get("appearance",{}).get("hair-color"),
+                occupation = i.get("work",{}).get("occupation"),
+                base = i.get("work",{}).get("base"),
+                group_affiliation = i.get("connections",{}).get("group-affiliation"),
+                relatives = i.get("connections",{}).get("relatives"),
+                image = i.get("image").get("url")
+            )
+        except Exception as e:
+            print(e)
 if __name__ == "__main__":
     main()
+    
--- db/init.sql
+++ db/init.sql
@@ -1,30 +1,30 @@
 CREATE DATABASE dc_heroes;
 \c dc_heroes;
-CREATE TABLE IF NOT EXISTS SUPER_HEROE(
+
+CREATE TABLE SUPER_HEROE(
     id SERIAL NOT NULL,
-    name varchar(50),
-    full_name varchar(200),
-    alter_egos varchar(200),
-    aliases varchar(200),
-    place_of_birth varchar(200),
-    first_appearance varchar(200),
-    publisher varchar(200),
-    alignment varchar(200),
-    gender varchar(200),
-    race varchar(200),
-    height varchar(200),
-    weight varchar(200),
-    eye_color varchar(200),
-    hair_color varchar(200),
-    occupation varchar(500),
-    base varchar(500),
-    group_affiliation varchar(500),
-    relatives varchar(500),
-    image varchar(500),
+    name VARCHAR(50),
+    full_name VARCHAR(100),
+    alter_egos VARCHAR(100),
+    aliases VARCHAR(100),
+    place_of_birth VARCHAR(100),
+    first_appearance VARCHAR(100),
+    publisher VARCHAR(100),
+    alignment VARCHAR(100),
+    gender VARCHAR(100),
+    race VARCHAR(100),
+    height VARCHAR(100),
+    weight VARCHAR(100),
+    eye_color VARCHAR(100),
+    hair_color VARCHAR(100),
+    occupation VARCHAR(500),
+    base VARCHAR(500),
+    group_affiliation VARCHAR(500),
+    relatives TEXT,
+    image VARCHAR(500),
     PRIMARY KEY(id)
 );
-
-CREATE TABLE IF NOT EXISTS SUPERHEROES_STATS(
+CREATE TABLE SUPERHEROES_STATS(
     id integer ,
     intelligence integer,
     strength integer,
